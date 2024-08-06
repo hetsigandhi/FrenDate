d18facdf48aabc274d2e1dd7070656cabd8eda5b
hetsigandhi <hetsigandhi@gmail.com>
2024-08-05 23:41:02 -0400

    FrenDate Website

---
+++ chatapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import MessagesListView, InboxView
+
+app_name = 'chatapp'
+
+urlpatterns = [
+    path('', MessagesListView.as_view(), name='message_list'),
+    path('inbox/<str:username>/', InboxView.as_view(), name='inbox'),
+]
